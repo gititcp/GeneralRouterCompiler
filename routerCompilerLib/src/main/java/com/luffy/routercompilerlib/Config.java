8f40063dee79a9f81217fd6a04ee5bf65f68a95a
diaoyonglong <diaoyonglong@aoji.cn>
2020-11-27 20:05:28 +0800

    first commit

---
+++ routerCompilerLib/src/main/java/com/luffy/routercompilerlib/Config.java
@@ -0,0 +1,8 @@
+package com.luffy.routercompilerlib;
+
+public class Config {
+    public static final String PACKAGE_NAME = "com.luffy.routerapilib.router";
+    public static final String FILE_PREFIX = "Router_";
+    public static final String ROUTER_MANAGER = "RouterManager";
+    public static final String ROUTER_MANAGER_METHOD = "setup";
+}
